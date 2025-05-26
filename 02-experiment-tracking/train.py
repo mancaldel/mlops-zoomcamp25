c28631df37941f0c2ccdaba7b6038b95b2b309f7
mancaldel <manuel.caldelgado@gmail.com>
2025-05-26 23:22:50 +0100

    add autolog to train script

--- 02-experiment-tracking/train.py
+++ 02-experiment-tracking/train.py
@@ -1,12 +1,15 @@
 import os
 import pickle
 import click
+import mlflow
 
+import mlflow.sklearn
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 
 
 def load_pickle(filename: str):
+    print(f"Loading pickle '{filename}'")
     with open(filename, "rb") as f_in:
         return pickle.load(f_in)
 
@@ -23,11 +26,30 @@ def run_train(data_path: str):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     rf = RandomForestRegressor(max_depth=10, random_state=0)
+    print("Fitting regressor")
     rf.fit(X_train, y_train)
+
+    print("Predicting on the validation set")
     y_pred = rf.predict(X_val)
 
     rmse = root_mean_squared_error(y_val, y_pred)
+    print(f"Validation RMSE: {rmse}")
 
 
 if __name__ == '__main__':
+
+    # Setup MLflow
+    mlflow.set_tracking_uri("sqlite:///../mlflow.db")
+    mlflow.set_experiment("nyc-taxi-experiment")
+
+    # with mlflow.start_run():
+
+    #     mlflow.set_tags(
+    #         {
+    #             "homework": 2,
+    #             "model": "RandomForest",
+    #         }
+    #     )
+    mlflow.sklearn.autolog(extra_tags={"homework": 2, "model": "RandomForest"})
+
     run_train()
