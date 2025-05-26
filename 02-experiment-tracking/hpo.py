dea29178905f6fd392627d753773dd0dfa4c1097
mancaldel <manuel.caldelgado@gmail.com>
2025-05-26 23:28:36 +0100

    add logging to hpo script

--- 02-experiment-tracking/hpo.py
+++ 02-experiment-tracking/hpo.py
@@ -35,10 +35,13 @@ def run_optimization(data_path: str, num_trials: int):
 
     def objective(params):
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = root_mean_squared_error(y_val, y_pred)
+        with mlflow.start_run():
+            mlflow.log_params(params=params)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = root_mean_squared_error(y_val, y_pred)
+            mlflow.log_metric("rmse", rmse)
 
         return {'loss': rmse, 'status': STATUS_OK}
 
